b97df68933338f4a0c95dea61f6b50f8277cd0b0
joe <hover2pi@gmail.com>
2011-06-01 17:13:34 +0000

    Added placeholder event boxes to the home page and put everything in place so I can pick out fonts and colors.

--- home.php
+++ home.php
@@ -4,7 +4,109 @@
 	
 	<div class="wrap">
 		
-		This is the jam
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be the title</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be a second title which is two lines</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box double-wide">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here is a double wide!</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be the title</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be the title</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be the title</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be the title</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be the title</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be the title</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<a href="#"><h2>Here be the title</h2></a>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+		</div><!-- END .event-box -->
+		
+		
+		
+		<div class="clear-both"></div>
 		
 	</div><!-- END .wrap -->
 
