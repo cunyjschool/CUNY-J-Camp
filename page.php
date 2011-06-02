494adb652bf24311d2c82f3baf196b308f0ab383
joe <hover2pi@gmail.com>
2011-06-02 21:05:55 +0000

    Added comments and made a first pass at comment style.

--- page.php
+++ page.php
@@ -6,38 +6,8 @@
 		
     	<div class="content">
     	    
-    	    
-    	    <div class="sidebar float-left">
-    	        
-    	        <div class="event-details">
-    	            
-    	            <img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
-    	            
-    	            <ul>
-    	                <li class="date">
-    	                    <div class="heading">Date:</div>
-    	                    <p>Wednesday, June 1, 2011</p>
-    	                </li>
-    	                <li class="time">
-    	                    <div class="heading">Time:</div>
-    	                    <p>6pm - 10pm</p>
-    	                </li>
-    	                <li class="prereq">
-    	                    <div class="heading">Prerequisite knowledge:</div>
-    	                    <p>Introduction to frying, advanced breading station</p>
-    	                </li>
-    	                <li class="equipment">
-    	                    <div class="heading">Equipment:</div>
-    	                    <p>thinly sliced chicken, flour, egg, breadcrumbs, vegetable oil</p>
-    	                </li>
-    	                <li class="location">
-    	                    <div class="heading">Location:</div>
-    	                    <p>CUNY J-School Kitchen</p>
-    	                </li>
-    	            </ul>
-    	        </div>
-    	    
-    	    </div><!-- END .sidebar -->
+    	    <?php get_sidebar(); ?>
+
     	    <div class="entry float-left">
     	            	        
 				<h3><?php the_title() ?></h3>
@@ -52,12 +22,14 @@
     	            <button>Add to Calendar</button>
     	        
     	        </div>
+    	        
+    	        <?php comments_template('', true); ?>
     	    
     	    </div><!-- END .entry -->
     	    
     	    <div class="clear-both"></div>
     	    
-    	</div><!-- END .content -->    
+    	</div><!-- END .content -->
 		
 	</div><!-- END .wrap -->
 
