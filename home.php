e1419fa87dbc5444ba09ef2f79cb0fa612613025
joe <hover2pi@gmail.com>
2011-06-02 14:03:16 +0000

    Moved featured double-wide event box to the second position.

--- home.php
+++ home.php
@@ -17,19 +17,6 @@
 			</div>
 		</div><!-- END .event-box -->
 		
-		<div class="event-box">
-			<div class="event-thumb">
-				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
-				<div class="event-category"><a href="#">cats</a></div>
-				<div class="event-date">Jan 12</div>
-			</div>
-			<div class="event-excerpt">
-				<div class="event-title"><a href="#">Here be the title</a></div>
-				<div class="event-byline">Posted by Ceiling Cat</div>
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
-			</div>
-		</div><!-- END .event-box -->
-			
 		<div class="event-box double-wide">
 			<div class="event-thumb">
 				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
@@ -38,6 +25,19 @@
 			</div>
 			<div class="event-excerpt">
 				<div class="event-title"><a href="#">Here is a double wide!</a></div>
+				<div class="event-byline">Posted by Ceiling Cat</div>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
+			</div>
+		</div><!-- END .event-box -->
+		
+		<div class="event-box">
+			<div class="event-thumb">
+				<img src="http://4.bp.blogspot.com/-gEEkwZRz26k/TatkHoEM6hI/AAAAAAAAFNg/l-ckU34vIW4/s400/ceiling%2Bcat.jpeg" />
+				<div class="event-category"><a href="#">cats</a></div>
+				<div class="event-date">Jan 12</div>
+			</div>
+			<div class="event-excerpt">
+				<div class="event-title"><a href="#">Here be the title</a></div>
 				<div class="event-byline">Posted by Ceiling Cat</div>
 				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat.</p>
 			</div>
