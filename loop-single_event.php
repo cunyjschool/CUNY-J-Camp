d3d1c721ef5143267f8a00c490e6fbadee12183e
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-06-13 22:18:08 +0000

    Continue styling and move the registration link down

--- loop-single_event.php
+++ loop-single_event.php
@@ -3,8 +3,6 @@
     <div class="post post-type-event">
         
         <?php if (have_posts()) : while ( have_posts()) : the_post(); ?>
-	
-			<a class="float-right event-registration button" href="#">Register</a>	
             	        
     		<h2><?php the_title() ?></h2>
 
@@ -17,6 +15,10 @@
 			<?php endif; ?>
 			
 			<div class="sidebar event-meta float-right">
+				
+				<div class="sidebar-item">
+					<a class="event-registration button" href="#">Register</a>	
+				</div>
 				
 				<div class="sidebar-item">
 					<h3>Date &amp; Time</h3>
@@ -34,7 +36,11 @@
 					<h3>Instructor<?php if ( count( $instructors ) > 1 ) { echo 's'; } ?></h3>
 					<?php
 						foreach ( $instructors as $instructor ) {
+							echo '<div class="instructor-item">';
 							echo '<h4>' . $instructor->name . '</h4>';
+							if ( !empty( $instructor->description ) ) 
+								echo '<p class="instructor-bio">' . $instructor->description . '</p>';
+							echo '</div>';
 						}
 					?>
 				</div>
